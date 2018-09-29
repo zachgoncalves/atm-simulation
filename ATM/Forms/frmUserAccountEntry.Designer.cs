59de666f345f1cd7356606e470ee27abd2adca24
zachgoncalves <zach.j.goncalves@gmail.com>
2018-09-28 20:36:43 -0400

    Add bank logo and design for frmUserAccount

--- ATM/Forms/frmUserAccountEntry.Designer.cs
+++ ATM/Forms/frmUserAccountEntry.Designer.cs
@@ -28,12 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblAccNum = new System.Windows.Forms.Label();
+            this.txtAccNum = new System.Windows.Forms.TextBox();
+            this.btnAccStart = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblAccNum
+            // 
+            this.lblAccNum.AutoSize = true;
+            this.lblAccNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAccNum.Location = new System.Drawing.Point(54, 159);
+            this.lblAccNum.Name = "lblAccNum";
+            this.lblAccNum.Size = new System.Drawing.Size(527, 24);
+            this.lblAccNum.TabIndex = 1;
+            this.lblAccNum.Text = "To begin, enter your five digit account number and press start.";
+            // 
+            // txtAccNum
+            // 
+            this.txtAccNum.Location = new System.Drawing.Point(187, 227);
+            this.txtAccNum.Name = "txtAccNum";
+            this.txtAccNum.Size = new System.Drawing.Size(111, 20);
+            this.txtAccNum.TabIndex = 2;
+            // 
+            // btnAccStart
+            // 
+            this.btnAccStart.BackColor = System.Drawing.Color.Green;
+            this.btnAccStart.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.btnAccStart.Location = new System.Drawing.Point(322, 222);
+            this.btnAccStart.Name = "btnAccStart";
+            this.btnAccStart.Size = new System.Drawing.Size(88, 28);
+            this.btnAccStart.TabIndex = 3;
+            this.btnAccStart.Text = "Start";
+            this.btnAccStart.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ATM.Properties.Resources.logo_small;
+            this.pictureBox1.Location = new System.Drawing.Point(31, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(303, 64);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // frmUserAccountEntry
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(636, 335);
+            this.Controls.Add(this.btnAccStart);
+            this.Controls.Add(this.txtAccNum);
+            this.Controls.Add(this.lblAccNum);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmUserAccountEntry";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frmUserAccountEntry";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblAccNum;
+        private System.Windows.Forms.TextBox txtAccNum;
+        private System.Windows.Forms.Button btnAccStart;
     }
 }
--- ATM/Forms/frmUserAccountEntry.cs
+++ ATM/Forms/frmUserAccountEntry.cs
@@ -16,5 +16,6 @@ namespace ATM.Forms
         {
             InitializeComponent();
         }
+
     }
 }
