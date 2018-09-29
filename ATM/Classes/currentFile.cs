f91cba1118521c73ee208247e6fae29dfb1755a2
zachgoncalves <zach.j.goncalves@gmail.com>
2018-09-28 22:29:23 -0400

    Implement currentFile class

--- ATM/Classes/currentFile.cs
+++ ATM/Classes/currentFile.cs
@@ -1,12 +1,90 @@
-﻿using System;
+﻿/* Temple University
+ * Component Based Software Design (CIS 3309) 
+ * Project 2: ATM
+ * Written by: Zachary Goncalves with code provided by Professor Frank Friedman
+ * 
+ * Description:
+ * This project is a simulation of the Game of Nim, in which players must not be the last player to take the last button
+ * This simulation keeps track of two players, the number of games played between these players, and the number of games
+ * each player has won.
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace ATM.Classes
 {
-    class currentFile
+    public class currentFile
     {
-    }
+        // currentFile class that handles searching the user database and returning data.
+        // Author: Professor Frank Friedman, with modifications by Zachary Goncalves
+        private string currentFilePath;
+        private System.IO.StreamReader currentFileSR;   // Reference variable of type SR
+        private int recordReadCount;
+
+        // Constructor with file path input
+        // Create instance of StreamReader class (type) and store reference
+        public currentFile
+            (string filePath)
+        {
+            recordReadCount = 0;
+            currentFilePath = filePath;
+            try
+            {
+                currentFileSR = new System.IO.StreamReader(currentFilePath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Cannot open file" + currentFilePath + "Terminate Program.",
+                            "Output File Connection Error.",
+                            MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            } // end Try
+        } // end currentFileClass 
+
+
+        // Read a record from the current file
+        public string getNextRecord(
+            ref Boolean endOfFileFlag)
+        {
+            string nextRecord;
+
+            endOfFileFlag = false;
+            nextRecord = currentFileSR.ReadLine();
+
+            if (nextRecord == null)
+            {
+                endOfFileFlag = true;
+            }
+            else
+            {
+                recordReadCount += 1;
+            } // end if
+
+            return (nextRecord);
+        } // end getNextRecord
+
+
+        // Get value of number of records read
+        public int getRecordsReadCount()
+        {
+            return recordReadCount;
+        } // end getecordsReadCount
+
+
+        // Close the input file
+        public void closeFile()
+        {
+            currentFileSR.Close();
+        } // end Sub
+
+
+        // Rewind the input file
+        public void rewindFile()
+        {
+            recordReadCount = 0;
+            currentFileSR.BaseStream.Seek(0, System.IO.SeekOrigin.Begin);
+        } // end rewindFile
+    } // end currentFileClass
 }
