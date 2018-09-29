831daefb83a880dc590a110f241b82e2ab001286
zachgoncalves <zach.j.goncalves@gmail.com>
2018-09-28 20:04:56 -0400

    Initial commit with form and classes created

--- ATM/Forms/frmTransactionEntry.cs
+++ ATM/Forms/frmTransactionEntry.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace ATM
+namespace ATM.Forms
 {
-    public partial class Form1 : Form
+    public partial class frmTransactionEntry : Form
     {
-        public Form1()
+        public frmTransactionEntry()
         {
             InitializeComponent();
         }
